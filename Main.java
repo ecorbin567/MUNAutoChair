883c4f09eabcb52ca44b7c81835fc3bf900e7675
Elise Corbin <88860277+ecorbin567@users.noreply.github.com>
2022-01-12 16:31:13 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
     Scanner myScanner = new Scanner(System.in);
-
     boolean checkOn = false;
+    Timer timer = new Timer();
 
     System.out.println("Enter total time in minutes (ex. 5min45sec = 5.75)");
     double totalTime = (myScanner.nextDouble() * 60);
@@ -13,17 +13,22 @@ class Main {
     myScanner.nextLine();
 
     int numSpeakers = (int) Math.floor(totalTime / speakingTime);
-    System.out.println("Enter " + numSpeakers + " country names");
+    System.out.println("Enter " + numSpeakers + " country names, enter stop to stop");
 
     String names[] = new String[numSpeakers + 1];
+    int count = 1;
+    String check1 = "test";
     for (int j = 1; j <= numSpeakers; j++) {
-      names[j] = myScanner.nextLine();
-      System.out.println(j + ": " + names[j]);
-    }
-
-    // long start = System.nanoTime();
-    // long end = System.nanoTime();
-    // long elapsedTime = end - start;
+      check1 = myScanner.nextLine();
+      if (check1.contains("stop")) {
+        break;
+      } // if stop
+      else {
+        names[j] = check1;
+        count++;
+        System.out.println(j + ": " + names[j]);
+      }
+    } // for
 
     System.out.println("Press 1 to begin when delegates are ready.");
     int check = myScanner.nextInt();
@@ -34,17 +39,30 @@ class Main {
     if (checkOn == true) {
       checkOn = false;
       for (int j = 1; j <= numSpeakers; j++) {
+        if (names[j]==null) {
+          break;
+        }
         System.out.println("Press 1 to begin when " + names[j] + " is ready.");
         check = myScanner.nextInt();
         myScanner.nextLine();
         if (check == 1) {
           checkOn = true;
-          System.out.println(names[j] + ", you have the floor for " + speakingTime + " seconds.");
-          wait(1000 * speakingTime - 10);
-          System.out.println(names[j] + ", you have ten seconds.");
-          wait(10000);
-          System.out.println("Thank you, " + names[j] + ".");
-          wait(2000);
+          int check2 = 0;
+          System.out.println(names[j] + ", you have the floor for " + speakingTime + " seconds. Press 1 to yield.");
+          
+          Warning task2= new Warning();
+          timer.schedule(task2, (speakingTime-10)*1000);
+          
+          Wait task = new Wait();
+          timer.schedule(task, speakingTime * 1000);
+
+          check2 = myScanner.nextInt();
+          myScanner.nextLine();
+          if (check2 == 1) {
+            task.cancel();
+            task2.cancel();
+            check2 = 0;
+          }
         } // if check==1
       } // for
 
@@ -57,20 +75,20 @@ class Main {
         checkOn = true;
         System.out.println("Enter more country names- enter stop to stop");
         String names2[] = new String[10];
-        int count = 1;
-        String check1 = "test";
+        count = 1;
+        check1 = "test";
         for (int j = 1; j <= 10; j++) {
-          names2[j] = myScanner.nextLine();
-          check1 = names2[j];
-          count++;
-          System.out.println(j + ": " + names2[j]);
-          //System.out.println(check1);
+          check1 = myScanner.nextLine();
           if (check1.contains("stop")) {
-            // System.out.println("breaking");
             break;
           } // if stop
+          else {
+            names2[j] = check1;
+            count++;
+            System.out.println(j + ": " + names2[j]);
+          }
         } // for
-        // System.out.println("broken");
+
         System.out.println("Press 1 to begin when delegates are ready.");
         check = myScanner.nextInt();
         myScanner.nextLine();
@@ -80,30 +98,35 @@ class Main {
         if (checkOn == true) {
           checkOn = false;
           for (int j = 1; j <= count; j++) {
+            if (names2[j]==null) {
+              break;
+            }
             System.out.println("Press 1 to begin when " + names2[j] + " is ready.");
             check = myScanner.nextInt();
             myScanner.nextLine();
             if (check == 1) {
               checkOn = true;
-              System.out.println(names2[j] + ", you have the floor for " + speakingTime + " seconds.");
-              wait(1000 * speakingTime - 10);
-              System.out.println(names2[j] + ", you have ten seconds.");
-              wait(10000);
-              System.out.println("Thank you, " + names2[j] + ".");
-              wait(2000);
+              int check2 = 0;
+              System.out.println(names2[j] + ", you have the floor for " + speakingTime + " seconds. Press 1 to yield.");
+
+              Warning task2= new Warning();
+              timer.schedule(task2, (speakingTime-10)*1000);
+
+              Wait task = new Wait();
+              timer.schedule(task, speakingTime * 1000);
+
+              check2 = myScanner.nextInt();
+              myScanner.nextLine();
+              if (check2 == 1) {
+                task.cancel();
+                task2.cancel();
+                check2 = 0;
+              }
             } // if check==1
           } // for
         } // if checkOn==true
       } // if check==1
     } // if checkOn==true
-  }// public static void main
-
-  public static void wait(int ms) {
-    try {
-      Thread.sleep(ms);
-    } catch (InterruptedException ex) {
-      Thread.currentThread().interrupt();
-    }
-  }
+  }// psv main
 
 }// class Main
